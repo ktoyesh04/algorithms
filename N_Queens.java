e209415880652bc5b9fc856efa1f40d24f2670a9
Kurapati Toyesh <ktoyesh04@gmail.com>
2023-08-16 19:40:23 +0530

    Modified N Queens

--- N_Queens.java
+++ N_Queens.java
@@ -1,65 +1,31 @@
 import java.util.Arrays;
 
 public class N_Queens {
-    static boolean isSafe(int row, int column, char[][] board){
-        
-        // checking in same row
-        for(int j=0; j<board.length; j++){
-            if(j!=column && board[row][j]=='Q') return false;
-        }
-
-        // checking in same column
-        for(int i=0; i<board.length; i++){
-            if(i!=row  && board[i][column]=='Q') return false;
-        }
-
-        // checking in upper left
-        for(int r=row, c=column; c>=0 && r>=0; c--, r--){
-            if(board[r][c]=='Q') return false;
-        }
-
-        // checking in upper right
-        for(int r=row, c=column; c<board.length && r>=0; c++, r--){
-            if(board[r][c]=='Q') return false;
-        }
-
-        // checking in lower left
-        for(int r=row, c=column; c>=0 && r<board.length; c--, r++){
-            if(board[r][c]=='Q') return false;
-        }        
-
-        // checking in lower right
-        for(int r=row, c=column; c<board.length && r<board.length; r++, c++){
-            if(board[r][c]=='Q') return false;
-        }
-
-        return true;
+    static boolean isSafe(int row, int column, int[] attackedRows, int[] attackedDiag1, int[] attackedDiag2){
+        return attackedRows[row]==0 && attackedDiag1[row+column]==0 && attackedDiag2[row-column+attackedRows.length-1]==0;
     }
 
-    static int count=0;
-    static void solveNQueens(char[][] board, int column){
+    static void solveNQueens(char[][] board, int column, int[] attackedRows, int[] attackedDiag1, int[] attackedDiag2){
         if(column==board.length){
-            System.out.println(count++);
-            for(char[] row : board) {
-                for(char ch : row){
-                    if(ch=='Q') System.out.print("Q ");
-                    else System.out.print(". ");
-                }
-                System.out.println();
-            }
+            for(char[] row: board) System.out.println(Arrays.toString(row));
             System.out.println();
             return;
         }
 
         for(int row=0; row<board.length; row++){
-            if(isSafe(row, column, board)){
+            if(isSafe(row, column, attackedRows, attackedDiag1, attackedDiag2)){
                 board[row][column] = 'Q';
-                solveNQueens(board, column+1);
+                attackedRows[row] = attackedDiag1[row+column] = attackedDiag2[row-column+board.length-1] = 1;
+
+                solveNQueens(board, column+1, attackedRows, attackedDiag1, attackedDiag2);
+
                 board[row][column] = '.';
+                attackedRows[row] = attackedDiag1[row+column] = attackedDiag2[row-column+board.length-1] = 0;
             }
         }
     }
     public static void main(String[] args) {
-        solveNQueens(new char[10][10], 0);
+        int n = 4;
+        solveNQueens(new char[n][n], 0, new int[n], new int[2*n-1], new int[2*n-1]);
     }
 }
